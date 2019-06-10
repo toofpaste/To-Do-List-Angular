eeddd694d4c6cfc79d91982982f67e1422093d98
Student <example@example.com>
2019-06-10 13:34:37 -0700

    To do list add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Task} from './models/task.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Angular';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 'HIGH'),
+    new Task('Begin brainstorming possible JavaScript group projects', 'LOW'),
+    new Task('Add README file to last few Angular repos on GitHub', 'MEDIUM')
+  ];
 }
